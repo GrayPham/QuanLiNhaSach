cbfa59fb62e4728b328bc39739e9f1280cca5364
Tam Nhu <tnhubh@gmail.com>
2022-05-10 23:41:39 +0700

    Xử lý sự kiện FormNhanVien: hiển thị datagridview và nút search

--- DBMS_FORM/FormNhanVien.cs
+++ DBMS_FORM/FormNhanVien.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using DBMS_FORM.BLL;
 namespace DBMS_FORM
 {
     public partial class FormNhanVien : Form
@@ -15,6 +15,21 @@ namespace DBMS_FORM
         public FormNhanVien()
         {
             InitializeComponent();
+        }
+        NhanVien nv = new NhanVien();
+
+        private void FormNhanVien_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = nv.hienThiNV();
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = nv.searchNV(txtfind.Text);
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Your search does not exist!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
