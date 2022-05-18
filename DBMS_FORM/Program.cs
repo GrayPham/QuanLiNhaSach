8e15b4f6cba43da989bf6347e00d47b3698e7a9c
kientin <tinhau1204@gmail.com>
2022-05-18 22:57:41 +0700

    them phan print cho doanhthu

--- DBMS_FORM/Program.cs
+++ DBMS_FORM/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace DBMS_FORM
 {
     static class Program
@@ -16,7 +17,7 @@ namespace DBMS_FORM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormThanhVien());
+            Application.Run(new TurnoverCheckForm());
         }
     }
 }
--- DBMS_FORM/TurnoverCheckForm.cs
+++ DBMS_FORM/TurnoverCheckForm.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using System.IO;
 
 namespace DBMS_FORM
 {
@@ -40,6 +42,76 @@ namespace DBMS_FORM
             if (radioButtonYear.Checked)
                 return 3;
             return 0;
+        }
+
+        private void ToDoc(DataGridView dGV, string filename)
+        {
+            string stOutput = "";
+            // Export titles:
+            string sHeaders = "";
+
+            for (int j = 0; j < dGV.Columns.Count; j++) 
+            {
+                
+                sHeaders = sHeaders.ToString() + Convert.ToString(dGV.Columns[j].HeaderText) + "\t";
+                stOutput += sHeaders + "\r\n";
+                Console.WriteLine('\t');
+            }
+            // Export data.
+            for (int i = 0; i < dGV.RowCount - 1; i++)
+            {
+                string stLine = "";
+                for (int j = 0; j < dGV.Rows[i].Cells.Count; j++)
+                    stLine = stLine.ToString() + Convert.ToString(dGV.Rows[i].Cells[j].Value) + "\t";
+                stOutput += stLine + "\r\n";
+                Console.WriteLine('\t');
+            }
+            Encoding utf16 = Encoding.GetEncoding(1254);
+            byte[] output = utf16.GetBytes(stOutput);
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(output, 0, output.Length); //write the encoded file
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (radioButtonDay.Checked)
+            {
+                SaveFileDialog sf = new SaveFileDialog();
+                sf.Filter = "Word 2007 or Higher(.docx) | *.docx";
+                sf.FileName = "exportday.doc"; 
+                if (sf.ShowDialog() == DialogResult.OK)
+                {
+                    ToDoc(DVGTurnOver, sf.FileName);
+                }
+            }
+            else if (radioButtonMouth.Checked)
+            {
+                
+                    SaveFileDialog sf = new SaveFileDialog();
+                    sf.Filter = "Word 2007 or Higher(.docx) | *.docx";
+                    sf.FileName = "exportmonth.doc";
+                    if (sf.ShowDialog() == DialogResult.OK)
+                    {
+                        ToDoc(DVGTurnOver, sf.FileName);
+                    }
+                
+            }
+            else if (radioButtonYear.Checked)
+            {
+                
+                    SaveFileDialog sf = new SaveFileDialog();
+                    sf.Filter = "Word 2007 or Higher(.docx) | *.docx";
+                    sf.FileName = "exportyear.doc";
+                    if (sf.ShowDialog() == DialogResult.OK)
+                    {
+                        ToDoc(DVGTurnOver, sf.FileName);
+                    }
+                
+            }
         }
     }
 }
